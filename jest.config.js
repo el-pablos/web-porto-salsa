55b25ca7368f7c7730d8da15d667c2825709a451
el-pablos <yeteprem.end23juni@gmail.com>
2026-03-18 14:34:02 +0700

    fix: tambah mock matchMedia di jest setup biar test ga error
    el-pablos <yeteprem.end23juni@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,39 @@
 /** @type {import('jest').Config} */
 const config = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterSetup: [],
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
-    '^framer-motion$': '<rootDir>/__mocks__/framer-motion.js',
-    '^react-intersection-observer$': '<rootDir>/__mocks__/react-intersection-observer.js',
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
+    "^framer-motion$": "<rootDir>/__mocks__/framer-motion.js",
+    "^react-intersection-observer$":
+      "<rootDir>/__mocks__/react-intersection-observer.js",
   },
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: {
-        jsx: 'react-jsx',
-        module: 'commonjs',
-        esModuleInterop: true,
-        allowJs: true,
-        paths: {
-          '@/*': ['./src/*'],
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          jsx: "react-jsx",
+          module: "commonjs",
+          esModuleInterop: true,
+          allowJs: true,
+          paths: {
+            "@/*": ["./src/*"],
+          },
         },
       },
-    }],
+    ],
   },
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-icons)/)',
+  transformIgnorePatterns: ["node_modules/(?!(react-icons)/)"],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/.next/",
+    "<rootDir>/e2e/",
   ],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/', '<rootDir>/e2e/'],
   collectCoverageFrom: [
-    'src/components/**/*.{ts,tsx}',
-    '!src/components/**/index.{ts,tsx}',
+    "src/components/**/*.{ts,tsx}",
+    "!src/components/**/index.{ts,tsx}",
   ],
 };
 
